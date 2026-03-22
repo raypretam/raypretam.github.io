9fa6297a5026df4215590096b5512ce3dfd038ac
raypretam <pretamroy309@gmail.com>
2026-03-22 19:32:23 +0000

    Deploying to gh-pages from @ raypretam/raypretam.github.io@bb40920a2dbff297e471e1c3ab55ee3bd636a50f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "raypretam/pretamray.github.io",
+    "data-repo": "raypretam/raypretam.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
